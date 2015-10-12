d672eb690ff4185b7e18ed249dced4842a2a5b6e
Nintenodblivion <AndrewArizala@gmail.com>
2015-10-11 22:13:45 -0700

    Assignment 1 Files

--- exception.h
+++ exception.h
@@ -1,20 +1,6 @@
 #include <iostream>
 
 using namespace std;
-/**************************************************************
-*	Author: Andrew Arizala
-* Date Created:			10/10/15
-* Last Modification Date:	10/10/15
-* Lab Number:			CST 211 Assignment 2
-* Filename:				exception.h
-*		 
-*
-*			Overview: To output messages for exceptions
-*			
-*			Input: A string
-*
-*			Output: None
-****************************************************************/
 
 class Exception{
 
@@ -30,7 +16,7 @@ public:
 	char getMessage() const;
 	void setMessage(const char *msg);
 
-	friend ostream &operator<<(ostream &out, const Exception &thing);
+	friend ostream &operator<<(ostream out, const Exception &thing);
 
 	
 
--- main.cpp
+++ main.cpp
@@ -1,36 +1,27 @@
-#include "Array2D.h"
 #include <iostream>
+#include "array.h"
 
 using namespace std;
 
-int main()
-{
-	try{
-		Array2D<int> thing;
-		Array2D<int> test(2, 5);
-		for (int idx = 0; idx < 2; idx++)
-		{
-			for (int id = 0; id < 5; id++)
-			{
-				test[idx][id] = ( (idx + 1) * (id + 1) ) - 1;
-				cout << test[idx][id] << " ";
-			}
-			cout << endl;
-		}
+int main(){
+	Array<int> thing(0, 10);
+	Array<int> thing2(thing);
+	Array<int> thing3 = thing;
+	thing[0];
+	thing.getLength();
+	thing.getStartIndex();
+	thing.lowerBound();
+	thing.numElements();
+	thing.setLength(5);
+	thing.setStartIndex(5);
+	thing.size();
+	thing.upperBound();
+	Exception test("thing");
+	Exception test2(test);
+	Exception test3 = test;
+	test.getMessage();
+	test.setMessage("woot");
 
-		const Array2D<int> test2(test);
-		cout << test2[0][0];
-
-		Array2D<int> test3 = test;
-
-		test.setColumn(4);
-		test.setRow(3);
-		test.getColumn();
-		test.getRow();
-	}
-	catch(Exception e){
-		cout << e.getMessage();
-	}
-	system("pause");
+	
 	return 0;
 }
